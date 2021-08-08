d9866330784dd770be18f83be402ee8fe290d003
Aman Ahmed <aman16.aa17@gmail.com>
2021-08-08 13:44:38 +0200

    Added Stream Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const PageOne = () => {
+  return (
+    <div>
+      Page 1
+      <a href="/pagetwo">Navigate to Page 2</a>
+    </div>
+  );
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      Page 2
+      <a href="/">Navigate to Page 1</a>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Route path="/" exact component={PageOne} />
+        <Route path="/pagetwo" component={PageTwo} />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
