87fa9a9a19bcbc42b561623768b06bc267389434
Aman Ahmed <aman16.aa17@gmail.com>
2021-08-11 11:23:26 +0200

    Updated StreamList

--- src/actions/index.js
+++ src/actions/index.js
@@ -23,8 +23,9 @@ export const signOut = () => {
 };
 
 /** Create a record */
-export const createStream = (formValues) => async (dispatch) => {
-  const response = await streams.post("/streams", formValues);
+export const createStream = (formValues) => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post("/streams", { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
 };
@@ -56,4 +57,3 @@ export const deleteStream = (id) => async (dispatch) => {
 
   dispatch({ type: DELETE_STREAM, payload: id });
 };
-
